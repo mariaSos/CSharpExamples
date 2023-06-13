e2ef6908afef2fc5a41c8c5f8f85b4a4f9e72465
maria_s_pr@mail.ru <maria_s_pr@mail.ru>
2023-06-13 16:50:12 +0500

    Задача 8

---
+++ Ex01_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+String str = "Все четные числа: ";
+for(int i = 1; i <= n; i++){
+if (i%2 == 0){
+    str = str + i + " ";
+}
+}
+System.Console.WriteLine(str);
